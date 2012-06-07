353aea97a2de71d0b05963fe0e78ce71c233c907
Dusan Krajcovic <dusan.krajcovic@gmail.com>
2012-06-07 13:16:12 +0200

    Vytvoreno nove tema rugbypointcz, zatim obsahuje pouze kopii prestashop-tematu

--- config/settings.inc.php
+++ config/settings.inc.php
@@ -1,22 +1,20 @@
 <?php
 define('_DB_SERVER_', 'localhost');
 define('_DB_TYPE_', 'MySQL');
-define('_DB_NAME_', 'prestashop');
+define('_DB_NAME_', 'rugbypointtest');
 define('_DB_USER_', 'prestashop');
 define('_DB_PASSWD_', 'prestashop');
 define('_DB_PREFIX_', 'ps_');
 define('_MYSQL_ENGINE_', 'InnoDB');
-define('__PS_BASE_URI__', '/shop/rugbypoint/');
+define('__PS_BASE_URI__', '/rugbypoint/');
 define('_PS_CACHING_SYSTEM_', 'MCached');
 define('_PS_CACHE_ENABLED_', '0');
 define('_MEDIA_SERVER_1_', '');
 define('_MEDIA_SERVER_2_', '');
 define('_MEDIA_SERVER_3_', '');
 define('_THEME_NAME_', 'prestashop');
-define('_COOKIE_KEY_', 'VaiiNZ7fXNtMsgGgP8xEU9jiyEs9ogys4tvqrgjF64ET8kFd7vJjraO1');
-define('_RIJNDAEL_KEY_', 'FRvbV6xEquveiA6BFk2HG6JuCH85GBvk');
-define('_COOKIE_IV_', 'SLjBxRcX');
-define('_RIJNDAEL_IV_', 'A+V9+Z265fiqN3TYy9oDWQ==');
-define('_PS_CREATION_DATE_', '2012-05-26');
+define('_COOKIE_KEY_', 'hBPPYD7b13NtUiHEnL882qQIrC4Xt9rNN6mdPDNK60JFGXFqEaURDizx');
+define('_COOKIE_IV_', 'Yc8qdKT9');
+define('_PS_CREATION_DATE_', '2012-06-07');
 define('_PS_VERSION_', '1.4.8.2');
 ?>
